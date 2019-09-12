badb841a8ff5be2c9d691d0cdd46cdcd098baac1
moxuse <moxuse@gmail.com>
2019-09-12 18:45:11 +0900

    add timer foreign

--- src/Main.ts
+++ src/Main.ts
@@ -1,6 +1,7 @@
 import Editor from "./Editor";
 import RenderView from "./RenderView";
 const THREE = require("three");
+import { timer } from "d3-timer";
 
 const renderView = new RenderView();
 new Editor(renderView);
@@ -8,3 +9,5 @@ new Editor(renderView);
 // global context
 window.port = renderView.port;
 window.THREE = THREE;
+window.d3 = {};
+window.d3.timer = timer;
