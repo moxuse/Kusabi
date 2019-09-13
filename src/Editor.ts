e2e8a55bbbe38ec4f172041bdbc296fbc8679ba5
moxuse <moxuse@gmail.com>
2019-09-13 20:51:31 +0900

    code blinking

--- src/Editor.ts
+++ src/Editor.ts
@@ -1,8 +1,10 @@
 require("ace-min-noconflict");
 require("ace-min-noconflict/mode-javascript");
+var Range = ace.require("ace/range").Range;
 import { Port } from "./Types";
 import Repl from "./Repl";
 import RenderView from "./RenderView";
+import { TweenLite } from "gsap";
 
 class Editor {
   editor = ace.edit("editor");
@@ -26,7 +28,13 @@ class Editor {
       bindKey: { win: "Ctrl-e", mac: "Ctrl-e" },
       exec: this.execCompile.bind(this)
     });
+    this.editor.commands.addCommand({
+      name: "showKeyboardShortcutsSelect",
+      bindKey: { win: "Ctrl-a", mac: "Ctrl-a" },
+      exec: this.highlightSelection.bind(this)
+    });
     this.editor.session.setOptions({ tabSize: 2, useSoftTabs: true });
+    this.editor.setOption("highlightActiveLine", false);
 
     window.postError = error => {
       const post = document.querySelector("#post form .text-area");
@@ -56,13 +64,57 @@ class Editor {
     if (!this.editor || !this.editor.session) {
       return;
     }
+    this.highlightSelection();
 
     if (0 < this.editor.session.getRowLength()) {
       const lines = this.editor.getSession().getValue();
-      // var currline = this.editor.getSelectionRange().start.row;
-      // var wholelinetxt = this.editor.session.getLine(currline);
       this.repl.compile(lines);
     }
+  }
+
+  /**
+   * code blink at execution
+   */
+  highlightSelection() {
+    const current = this.editor.getCursorPosition().row;
+    let up = current;
+    let bottom = current;
+    while (true) {
+      if (this.editor.session.getLine(up).length > 0) {
+        up = up - 1;
+        if (up < 0) {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+    while (true) {
+      if (this.editor.session.getLine(bottom).length > 0) {
+        bottom = bottom + 1;
+      } else {
+        break;
+      }
+    }
+    let marker = this.editor.session.addMarker(
+      new Range(up + 1, 0, bottom - 1, 1),
+      "execMarker",
+      "fullLine"
+    );
+    setTimeout(() => {
+      const elems = document.getElementsByClassName("execMarker");
+      if (elems && elems.length > 0) {
+        const array = Array.from(elems);
+        array.forEach(element => {
+          TweenLite.to(element, 0.5, {
+            opacity: 0,
+            onComplete: () => {
+              this.editor.session.removeMarker(marker);
+            }
+          });
+        });
+      }
+    }, 20);
   }
 
   postWindow(msg: string) {
