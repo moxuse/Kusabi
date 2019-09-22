da1151881259acc7ff0b28ecec431b87d403e400
moxuse <moxuse@gmail.com>
2019-09-22 10:21:47 +0900

    added postprocessing target

--- src/Types.ts
+++ src/Types.ts
@@ -1,12 +1,27 @@
-import { Scene, Mesh, Texture, Object3D, WebGLRenderTarget } from "three";
-
-// interface TextureNode {}
-// interface EffectNode {}
-// interface RendererTaregtNode {}
-
-// export type YodakaNode = Object3D | Mesh | Texture;
+import { Scene, Object3D, WebGLRenderTarget } from "three";
+import { Effect } from "postprocessing";
 
 export type Port = {
   scene: Scene;
   targets: Array<{ target: WebGLRenderTarget; scene: Object3D }>;
+  postEffects: Array<{ effect: Effect; renderToScreen: Boolean }>;
 };
+
+export function postEffectsPortProxy(
+  array: Array<{ effect: Effect; renderToScreen: Boolean }>,
+  callback: (
+    target: { effect: any; renderToScreen: Boolean }[],
+    length: number
+  ) => void
+) {
+  const p = new Proxy(array, {
+    deleteProperty: (target, property) => {
+      return Reflect.deleteProperty(target, property);
+    },
+    set: (target, property, val, receiver) => {
+      callback(target, target.length);
+      return Reflect.set(target, property, val, receiver);
+    }
+  });
+  return p;
+}
