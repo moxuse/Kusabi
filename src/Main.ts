5edfcab72a88bd62d66aa58516e815de84117d74
moxuse <moxuse@gmail.com>
2019-11-18 16:12:06 +0900

    refine operators

--- src/Main.ts
+++ src/Main.ts
@@ -32,3 +32,5 @@ window.THREE.BloomEffect = BloomEffect;
 window.THREE.NoiseEffect = NoiseEffect;
 window.THREE.BokehEffect = BokehEffect;
 window.THREE.DepthEffect = DepthEffect;
+
+console.log('three version ::',  THREE.REVISION);
