38d12c09f2032baffefd82fd8361eab6d151c84c
moxuse <moxuse@gmail.com>
2020-05-30 17:53:29 +0900

    exportを連結

--- src/Exports/postprocess.sh
+++ src/Exports/postprocess.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 {
     cp src/Exports/index.html dist/index.html &&
-    cp src/Yodaka/index.js dist/index.js
-    # $(npm bin)/uglifyjs dist/index.js dist/Main.js -o bundle.js
+    cp src/Yodaka/index.js dist/index.js &&
+    cat dist/Main.js > dist/bundle.js && cat dist/index.js >> dist/bundle.js &&
+    rm dist/Main.js && rm dist/index.js
 }
